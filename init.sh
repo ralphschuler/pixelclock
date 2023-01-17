87ba38b30d145eb0845048635d3241599366f3c1
Ralph Schuler <ralph.schuler@nyphon.de>
2023-01-17 20:41:28 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
+ACTION='\033[1;90m'
+FINISHED='\033[1;96m'
+READY='\033[1;92m'
+NOCOLOR='\033[0m' # No Color
+ERROR='\033[0;31m'
 
-git reset --hard
-git pull --dry-run | grep -q -v 'Already up-to-date.' && updated=1 || updated=0
+echo
+echo -e ${ACTION}Checking Git repo
+echo -e =======================${NOCOLOR}
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$BRANCH" != "master" ]; then
+    echo -e ${ERROR}Not on master. Aborting. ${NOCOLOR}
+    echo
+    exit 0
+fi
 
-if [ $updated -eq 1 ]; then
-    echo "Updated pixelclock"
-    echo "Installing dependencies"
+git fetch
+HEADHASH=$(git rev-parse HEAD)
+UPSTREAMHASH=$(git rev-parse master@{upstream})
+
+if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
+    echo -e ${ERROR}Not up to date with origin. Updating.${NOCOLOR}
+    git reset --hard origin/master
+    git pull
+
     npm install
-    echo "Building pixelclock"
     npm run build
-    echo "Restarting pixelclock"
+
     $*
 else
-    echo "Starting pixelclock"
+    echo -e ${FINISHED}Current branch is up to date with origin/master.${NOCOLOR}
     sudo npm run dev
 fi
