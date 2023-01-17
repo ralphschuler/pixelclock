349761d2fb5593f90714b268625d86124d6d0ce6
Ralph Schuler <ralph.schuler@nyphon.de>
2023-01-17 20:42:03 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,19 +9,19 @@ echo
 echo -e ${ACTION}Checking Git repo
 echo -e =======================${NOCOLOR}
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [ "$BRANCH" != "master" ]; then
-    echo -e ${ERROR}Not on master. Aborting. ${NOCOLOR}
+if [ "$BRANCH" != "main" ]; then
+    echo -e ${ERROR}Not on main. Aborting. ${NOCOLOR}
     echo
     exit 0
 fi
 
 git fetch
 HEADHASH=$(git rev-parse HEAD)
-UPSTREAMHASH=$(git rev-parse master@{upstream})
+UPSTREAMHASH=$(git rev-parse main@{upstream})
 
 if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
     echo -e ${ERROR}Not up to date with origin. Updating.${NOCOLOR}
-    git reset --hard origin/master
+    git reset --hard origin/main
     git pull
 
     npm install
@@ -29,6 +29,6 @@ if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
 
     $*
 else
-    echo -e ${FINISHED}Current branch is up to date with origin/master.${NOCOLOR}
+    echo -e ${FINISHED}Current branch is up to date with origin/main.${NOCOLOR}
     sudo npm run dev
 fi
