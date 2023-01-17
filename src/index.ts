d8191d83e784af1cc57015fa84639fe74b0f9cef
Ralph Schuler <ralph.schuler@nyphon.de>
2023-01-17 19:04:21 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import { Matrix } from "./Matrix";
 
+function getPixelId(x: number, y: number) {
+  if (x % 2 === 0) {
+    return x / 2 + y * (rows + 1);
+  } else {
+    return rows + 1 - x + x / 2 + y * (rows + 1);
+  }
+}
+
 const rows = 5;
 const columns = 17;
 const matrix = new Matrix({
   rows: rows,
   columns: columns,
   ledCount: 90,
-  getPixelId: (x, y) => {
-    if (y % 2 === 0) {
-      return y / 2 + x * (rows + 1);
-    } else {
-      return rows + 1 - y + y / 2 + x * (rows + 1);
-    }
-  },
+  getPixelId: getPixelId,
 });
 
 function fillScreen(color: number) {
@@ -44,7 +46,7 @@ function rgbToInt(r: number, g: number, b: number) {
 
 let offset = 0;
 setInterval(() => {
-  console.log(new Date().toISOString(), "loop");
+  console.log(new Date().toISOString(), "loop", getPixelId(5,1));
   for (let x = 0; x < rows; x++) {
     for (let y = 0; y < columns; y++) {
       matrix.setPixel(x, y, colorwheel((x + y + offset) % 256));
