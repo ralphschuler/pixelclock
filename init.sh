1254fc97717a6d364d258716aa4381a82a612502
Ralph Schuler <ralph.schuler@nyphon.de>
2023-01-17 23:58:20 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,96 +5,93 @@ READY='\033[1;92m'
 NOCOLOR='\033[0m'
 ERROR='\033[0;31m'
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>init.log 2>&1
-
+exec 3>&1 1>>./init.log 2>&1
 
 trap trapint SIGINT SIGTERM
 function trapint {
-    echo
-    echo -e ${ERROR}Caught SIGINT. Exiting...${NOCOLOR}
-    echo -e =======================${NOCOLOR}
+    echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ERROR}Caught SIGINT. Exiting...${NOCOLOR} | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
     exit 0
 }
 
 if (( $EUID != 0 )); then
-    echo
-    echo -e ${ERROR}Please run as root.${NOCOLOR}
-    echo -e =======================${NOCOLOR}
+    echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ERROR}Please run as root.${NOCOLOR} | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
     exit 1
 fi
 
 if npm run is-running; then
-    echo
-    echo -e ${FINISHED}Pixelclock is running.${NOCOLOR}
-    echo -e =======================${NOCOLOR}
+    echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") ${FINISHED}Pixelclock is running.${NOCOLOR} | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
 else
-    echo
-    echo -e ${ERROR}Pixelclock is not running.${NOCOLOR}
-    echo -e =======================${NOCOLOR}
+    echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ERROR}Pixelclock is not running.${NOCOLOR} | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
 
-    echo
-    echo -e ${ACTION}Starting pixelclock...${NOCOLOR}
-    echo -e =======================${NOCOLOR}
+    echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Starting pixelclock...${NOCOLOR} | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
     npm run start
 fi
 
 
-echo
-echo -e ${ACTION}Checking for main branch...${NOCOLOR}
-echo -e =======================${NOCOLOR}
+echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Checking for main branch...${NOCOLOR} | tee /dev/fd/3
+echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [ "$BRANCH" != "main" ]; then
-    echo -e ${ERROR}Not on main. Aborting update check.${NOCOLOR}
-    echo
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ERROR}Not on main. Aborting update check.${NOCOLOR} | tee /dev/fd/3
+    echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
     exit 0
 fi
 
 
 while true; do
-    echo
-    echo -e ${ACTION}Checking for updates...${NOCOLOR}
-    echo -e =======================${NOCOLOR}
+    echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Checking for updates...${NOCOLOR} | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
     git fetch
     HEADHASH=$(git rev-parse HEAD)
     UPSTREAMHASH=$(git rev-parse main@{upstream})
     if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
-        echo -e ${ERROR}Not up to date with origin. Updating.${NOCOLOR}
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ERROR}Not up to date with origin. Updating.${NOCOLOR} | tee /dev/fd/3
 
-        echo
-        echo -e ${ACTION}Resetting to origin/main...${NOCOLOR}
-        echo -e =======================${NOCOLOR}
+        echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Resetting to origin/main...${NOCOLOR} | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
         git reset --hard origin/main
 
-        echo
-        echo -e ${ACTION}Installing dependencies...${NOCOLOR}
-        echo -e =======================${NOCOLOR}
+        echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Installing dependencies...${NOCOLOR} | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
         npm install
 
-        echo
-        echo -e ${ACTION}Building pixelclock...${NOCOLOR}
-        echo -e =======================${NOCOLOR}
+        echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Building pixelclock...${NOCOLOR} | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
         npm run build
 
-        echo
-        echo -e ${ACTION}Restarting pixelclock... ${NOCOLOR}
-        echo -e =======================${NOCOLOR}
+        echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Restarting pixelclock... ${NOCOLOR} | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
         npm run restart
 
-        echo
-        echo -e ${ACTION}Restarting parent process...${NOCOLOR}
-        echo -e =======================${NOCOLOR}
+        echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Restarting parent process...${NOCOLOR} | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
         exec ./init.sh
         exit 0
     else
-        echo
-        echo -e ${FINISHED}Current branch is up to date with origin/main.${NOCOLOR}
-        echo -e =======================${NOCOLOR}
+        echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") ${FINISHED}Current branch is up to date with origin/main.${NOCOLOR} | tee /dev/fd/3
+        echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
     fi
 
-    echo
-    echo -e ${ACTION}Next update check in 5 minutes.${NOCOLOR}
-    echo -e =======================${NOCOLOR}
+    echo $(date "+%Y-%m-%d %H:%M:%S") | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") ${ACTION}Next update check in 5 minutes.${NOCOLOR} | tee /dev/fd/3
+    echo -e $(date "+%Y-%m-%d %H:%M:%S") =======================${NOCOLOR} | tee /dev/fd/3
     sleep 300
 done
