cbb16e67580198fda589143f5fb04c54e49cedf3
Ralph Schuler <ralph.schuler@nyphon.de>
2023-01-17 17:20:03 +0100

    add examples

--- src/index.ts
+++ src/index.ts
@@ -28,15 +28,18 @@ function rgbToHex(r: number, g: number, b: number) {
 }
 
 (async () => {
-  while (true) {
+    
+  const loop = () => {
     for (let r = 0; r < 256; r++) {
       for (let g = 0; g < 256; g++) {
         for (let b = 0; b < 256; b++) {
           fillScreen(rgbToHex(r, g, b));
           matrix.render();
-          await new Promise((resolve) => setTimeout(resolve, 10));
         }
       }
     }
   }
+
+  setInterval(loop, 1000 / 30);
+
 })();
