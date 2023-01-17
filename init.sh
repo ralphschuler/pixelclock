8b86dfcc64f36c7cc33e29f7093c06b70d22bdd1
Ralph Schuler <ralph.schuler@nyphon.de>
2023-01-17 20:16:50 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git_had_update=$(git pull 2>&1 | grep -c 'Already up-to-date')
+
+if [ $git_had_update -ne 0 ]; then
+    echo "Updated pixelclock"
+    $*
+else
+    echo "Starting pixelclock"
+    node dist/index.js
+fi
