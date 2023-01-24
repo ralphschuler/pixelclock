27f5de4ed03598eddbbce10c11b2c49668a7ea8b
Ralph Schuler <ralph.schuler@nyphon.de>
2023-01-24 20:57:31 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -25,148 +25,238 @@ BG_CYAN='\033[46m'
 BG_ORANGE='\033[43m'
 RESET='\033[0m'
 
-# Logging
-if [ ! -d "./logs" ]; then
-    mkdir ./logs
-fi
-
-# Rotate logs
-if [ -f "./logs/latest.log" ]; then
-    FILEDATE=$(date -r ./logs/latest.log "+%Y-%m-%d")
-    FILENUMBER=0
-    while [ -f "./logs/${FILEDATE}_${FILENUMBER}.log" ]; do
-        FILENUMBER=$((FILENUMBER+1))
-    done
-    mv ./logs/latest.log ./logs/${FILEDATE}_${FILENUMBER}.log
-fi
-
-exec 3>&1 1>>./logs/latest.log 2>&1
-function log {
-    echo -e "${GREY} =======================${RESET}" | tee /dev/fd/3
-    echo -e "${WHITE}$(date "+%Y-%m-%d %H:%M:%S")${RESET}" | tee /dev/fd/3
-    echo -e "$1$2${RESET}" | tee /dev/fd/3
-    echo -e "" | tee /dev/fd/3
-}
-
-function error_exit {
-    echo -e "${RED}${1}\nExiting..." | tee /dev/fd/3
-    exit 1
-}
-
-function success {
-    echo -e "${GREEN}$1${RESET}" | tee /dev/fd/3
-}
-
-# Traps
-trap trapint SIGINT SIGTERM
-function trapint {
-    log ${RED} "Caught SIGINT or SIGTERM.\nExiting..."
-    exit 0
-}
-
-function start_service {
-    log ${WHITE} "Checking installation..."
-    log ${WHITE} "Check service status..."
-    SERVICE_PID=$(yarn pid)
-    if [ "${SERVICE_PID:=-1}" != -1 ]; then
-        log ${GREEN} "Service is running."
-    else
-        log ${YELLOW} "Service is not running.\nStarting..."
-        yarn start && success "Successfully started service." || error_exit "Failed to start service."
-    fi
-}
-
-function install_service {
-    log ${WHITE} "Pulling latest changes..."
-    git reset --hard origin/main && success "Successfully pulled latest changes" || error_exit "Failed to pull latest changes."
-
-    log ${WHITE} "Installing dependencies..."
-    yarn install --ci && success "Successfully installed dependencies" || error_exit "Failed to install dependencies."
-
-    log ${WHITE} "Building service..."
-    yarn build && success "Successfully built service" || error_exit "Failed to build service."
-
-    log ${WHITE} "Creating service..."
-    yarn startup && success "Successfully created service" || error_exit "Failed to create service."
-
-    log ${WHITE} "Saving pm2 config..."
-    yarn save && success "Successfully saved pm2 config" || error_exit "Failed to save pm2 config."
-
-    log ${WHITE} "Restarting service..."
-    yarn restart && success "Successfully restarted service" || error_exit "Failed to restart service."
-    exec $1 $@; exit 0;
-}
-
-# Check if root
-if (( $EUID != 0 )); then
-    error_exit "Please run as root."
-fi
-
-# Respond to --help and -h (if set, show the help message and exit)
-if [[ "$@" == *"--help"* ]] && [[ "$@" == *"-h"* ]]; then
-    log ${WHITE} "You can use the following arguments:\n--help,\t-h\tShow this help message.\n--quiet,\t-q\tDon't show the logo.\n--version,\t-v\tShow the version and exit."
-    exit 0
-fi
-
-# Respond to --version and -v (if set, show the version and exit)
-AUTHOR=$(git log -1 --format="%an")
-REPOSITORY=$(git config --get remote.origin.url)
-LAST_UPDATED=$(git log -1 --format="%cd")
-VERSION=$(git describe --tags --exact-match 2> /dev/null || git symbolic-ref -q --short HEAD || git rev-parse --short HEAD) # tag > branch > commit
-if [[ "$@" == *"--version"* ]] && [[ "$@" == *"-v"* ]]; then
-    # check if version is main
-    [[ ${VERSION:} == "main" ]] && VERSION="${GREEN}${VERSION}${RESET}" || VERSION="${RED}${VERSION}${RESET}"
-    log ${WHITE} "Version: ${VERSION}\nRepository: ${REPOSITORY}\nLast updated: ${LAST_UPDATED}‚\nAuthor: ${AUTHOR}"
-    exit 0
-fi
-
+# Utility functions
 function show_banner {
-    # Respond to --quiet and -q (if set, don't show the logo and continue)
-    if [[ "$@" != *"--quiet"* ]] && [[ "$@" != *"-q"* ]]; then
-        echo -e "${WHITE}===================================================${RESET}" | tee /dev/fd/3
-        echo -e "${GREY} _____ ${RED}_${RESET}         ${BLUE} _${RESET}${GREY}    _____${RESET} ${WHITE}_           ${RESET}${RED} _    ${RESET}" | tee /dev/fd/3
-        echo -e "${GREY}|  __ ${RED}(_)${RESET}        ${BLUE}| |${RESET}${GREY}  / ____${RESET}${WHITE}| |          ${RESET}${RED}| |   ${RESET}" | tee /dev/fd/3
-        echo -e "${GREY}| |__) |${CYAN}__  __${RESET}___${BLUE}| |${RESET}${GREY} | |    ${RESET}${WHITE}| | ${RESET}${PURPLE}___${RESET}  ${CYAN} ___${RED}| | __${RESET}" | tee /dev/fd/3
-        echo -e "${GREY}|  ___/ ${CYAN}\ \/ /${RESET} _ \\\\${BLUE} |${RESET}${GREY} | |    ${RESET}${WHITE}| |${RESET}${PURPLE}/ _ \\\\${RESET} ${CYAN}/ __${RED}| |/ /${RESET}" | tee /dev/fd/3
-        echo -e "${GREY}| |   | ${CYAN}|>  < ${RESET} __/${BLUE} | ${RESET}${GREY}| |____${RESET}${WHITE}| |${RESET}${PURPLE} (_) ${RESET}${CYAN}| (__${RED}|   < ${RESET}" | tee /dev/fd/3
-        echo -e "${GREY}|_|   |_${CYAN}/_/\_\\\\${RESET}___${BLUE}|_|${RESET}${GREY}  \_____${RESET}${WHITE}|_|${RESET}${PURPLE}\___/${RESET} ${CYAN}\___${RED}|_|\_\\\\${RESET}" | tee /dev/fd/3
-        echo -e "${WHITE}===================================================${RESET}" | tee /dev/fd/3
+  if [[ "$@" != *"--quiet"* ]] && [[ "$@" != *"-q"* ]]; then
+    log "${WHITE}===================================================${RESET}"
+    log "${GREY} _____ ${RED}_${RESET}         ${BLUE} _${RESET}${GREY}    _____${RESET} ${WHITE}_           ${RESET}${RED} _    ${RESET}"
+    log "${GREY}|  __ ${RED}(_)${RESET}        ${BLUE}| |${RESET}${GREY}  / ____${RESET}${WHITE}| |          ${RESET}${RED}| |   ${RESET}"
+    log "${GREY}| |__) |${CYAN}__  __${RESET}___${BLUE}| |${RESET}${GREY} | |    ${RESET}${WHITE}| | ${RESET}${PURPLE}___${RESET}  ${CYAN} ___${RED}| | __${RESET}"
+    log "${GREY}|  ___/ ${CYAN}\ \/ /${RESET} _ \\\\${BLUE} |${RESET}${GREY} | |    ${RESET}${WHITE}| |${RESET}${PURPLE}/ _ \\\\${RESET} ${CYAN}/ __${RED}| |/ /${RESET}"
+    log "${GREY}| |   | ${CYAN}|>  < ${RESET} __/${BLUE} | ${RESET}${GREY}| |____${RESET}${WHITE}| |${RESET}${PURPLE} (_) ${RESET}${CYAN}| (__${RED}|   < ${RESET}"
+    log "${GREY}|_|   |_${CYAN}/_/\_\\\\${RESET}___${BLUE}|_|${RESET}${GREY}  \_____${RESET}${WHITE}|_|${RESET}${PURPLE}\___/${RESET} ${CYAN}\___${RED}|_|\_\\\\${RESET}"
+    log "${WHITE}===================================================${RESET}"
+  fi
+}
+
+function check_if_running {
+  if [ -f "/var/run/pixelclock.pid" ]; then
+    PID=$(cat /var/run/pixelclock.pid)
+    if ps -p $PID > /dev/null; then
+      return 0
     fi
+  fi
+
+  return 1
+}
+
+function check_if_root {
+  if [ "$EUID" -ne 0 ]; then
+    log "${RED}This script must be run as root. Exiting...${RESET}"
+    exit 1
+  fi
 }
 
 function check_for_updates {
-    log ${WHITE} "Checking for main branch..."
-    BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    if [ "$BRANCH" != "main" ]; then
-        error_exit "Only the main branch can be updated.\nExiting..."
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  if [[ "$BRANCH" == "main" ]]; then
+    git fetch origin main || log "${RED}Failed to fetch updates. Skipping...${RESET}"
+    HEADHASH=$(git rev-parse HEAD)
+    UPSTREAMHASH=$(git rev-parse main@{upstream})
+    if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
+      log "${YELLOW}There are updates available.${RESET}"
+      return 1
     fi
-    while true; do
-        log ${WHITE} "Checking for updates..."
-        git fetch origin main || error_exit "Failed to fetch updates."
-        HEADHASH=$(git rev-parse HEAD)
-        UPSTREAMHASH=$(git rev-parse main@{upstream})
-        if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
-            log ${YELLOW} "Update found.\nUpdating..."
-            install_service
-            start_service
-        else
-            log ${GREEN} "No updates found."
-        fi
+  fi
 
-        log ${WHITE} "Waiting 60 seconds..."
-        sleep 60
-    done
+  return 0
+}
+
+function rotate_logs {
+  if [ -f "./logs/latest.log" ]; then
+      FILEDATE=$(date -r ./logs/latest.log "+%Y-%m-%d")
+      FILENUMBER=0
+      while [ -f "./logs/${FILEDATE}_${FILENUMBER}.log" ]; do
+          FILENUMBER=$((FILENUMBER+1))
+      done
+      mv ./logs/latest.log ./logs/${FILEDATE}_${FILENUMBER}.log
+  fi
+}
+
+function trapint {
+  log "${RED}Exiting...${RESET}"
+  exit 0
+}
+
+function log {
+  echo -e "$@" | tee /dev/fd/3
+  echo -e "$@"
+}
+
+#setup functions
+function setup_logs {
+  if [ ! -d "./logs" ]; then
+    mkdir ./logs
+  fi
+  exec 3>&1 1>>./logs/latest.log 2>&1
+}
+
+function setup_trapint {
+  trap trapint SIGINT SIGTERM
+}
+
+# Service functions
+function start {
+  if check_if_running; then
+    log "${GREEN}Starting PixelClock...${RESET}"
+    node ./src/index.js &
+    PID=$!
+    echo $PID > /var/run/pixelclock.pid
+    log "${GREEN}PixelClock started with PID ${PID}.${RESET}"
+  else
+    log "${RED}PixelClock is already running.${RESET}"
+  fi
+}
+
+function stop {
+  if check_if_running; then
+    log "${RED}PixelClock is not running.${RESET}"
+  else
+    log "${GREEN}Stopping PixelClock...${RESET}"
+    kill -9 $PID
+    rm /var/run/pixelclock.pid
+    log "${GREEN}PixelClock stopped.${RESET}"
+  fi
+}
+
+function restart {
+  stop
+  start
+}
+
+function status {
+  if check_if_running; then
+    log "${RED}PixelClock is not running.${RESET}"
+  else
+    log "${GREEN}PixelClock is running with PID ${PID}.${RESET}"
+  fi
+}
+
+function install {
+  if check_if_running; then
+    log "${GREEN}Installing PixelClock...${RESET}"
+    cp ./pixelclock.service /etc/systemd/system/pixelclock.service
+    systemctl daemon-reload
+    systemctl enable pixelclock.service
+    log "${GREEN}PixelClock installed.${RESET}"
+  else
+    log "${RED}PixelClock is already running.${RESET}"
+  fi
+}
+
+function uninstall {
+  if check_if_running; then
+    log "${GREEN}Uninstalling PixelClock...${RESET}"
+    systemctl disable pixelclock.service
+    rm /etc/systemd/system/pixelclock.service
+    systemctl daemon-reload
+    log "${GREEN}PixelClock uninstalled.${RESET}"
+  else
+    log "${RED}PixelClock is already running.${RESET}"
+  fi
+}
+
+function usage {
+  log "${WHITE}Usage: ${0} [ACTION] [FLAGS]${RESET}"
+  log "${WHITE}Actions:${RESET}"
+  log "${WHITE}  start${RESET} - Start the PixelClock service"
+  log "${WHITE}  stop${RESET} - Stop the PixelClock service"
+  log "${WHITE}  restart${RESET} - Restart the PixelClock service"
+  log "${WHITE}  status${RESET} - Check the status of the PixelClock service"
+  log "${WHITE}  install${RESET} - Install the PixelClock service"
+  log "${WHITE}  uninstall${RESET} - Uninstall the PixelClock service"
+  log "${WHITE}  update${RESET} - Update the PixelClock service"
+  log ""
+  log "${WHITE}Flags:${RESET}"
+  log "${WHITE}  -q, --quiet${RESET} - Suppress all output"
+  log "${WHITE}  -h, --help${RESET} - Show this help message"
+  log "${WHITE}  -v, --version${RESET} - Show the version of PixelClock"
+  log ""
+
+  exit 0
+}
+
+function version {
+  AUTHOR=$(git log -1 --format="%an")
+  REPOSITORY=$(git config --get remote.origin.url)
+  LAST_UPDATED=$(git log -1 --format="%cd")
+  VERSION=$(git describe --tags --exact-match 2> /dev/null || git symbolic-ref -q --short HEAD || git rev-parse --short HEAD) # tag > branch > commit
+  [[ "${VERSION}" == "main" ]] && VERSION="${GREEN}${VERSION}${RESET}" || VERSION="${RED}${VERSION}${RESET}"
+  log "${WHITE}Version: ${VERSION}\nRepository: ${REPOSITORY}\nLast updated: ${LAST_UPDATED}‚\nAuthor: ${AUTHOR}"
+  
+  exit 0
+}
+
+function update {
+  log "${GREEN}Updating PixelClock...${RESET}"
+  stop
+  git reset --hard origin/main || log "${RED}Failed to reset to origin/main.${RESET}"; exit 1
+  yarn install || log "${RED}Failed to install dependencies.${RESET}"; exit 1
+  yarn build || log "${RED}Failed to build PixelClock.${RESET}"; exit 1
+  start
+  log "${GREEN}PixelClock updated.${RESET}"
 }
 
 function main {
-    show_banner
-    log ${WHITE} "Initializing..."
-    log ${GREY} "Version: ${VERSION} | Startup: $(date "+%Y-%m-%d %H:%M:%S") | PID: $$${RESET}"
-    install_service
-    start_service
-    check_for_updates
+  setup_trapint
+  setup_logs
 
-    yarn logs
+  show_banner
+
+  if [[ "$1" == "" ]]; then
+    usage $@
+  fi
+
+  if [[ "$@" == *"--version"* ]] && [[ "$@" == *"-v"* ]]; then
+    version $@
+  fi
+
+  if [[ "$@" == *"--help"* ]] && [[ "$@" == *"-h"* ]]; then
+    usage $@
+  fi
+
+  check_if_root
+
+  if [[ "$@" == *"--no-updates"* ]] && [[ "$@" == *"-n"* ]]; then
+    check_for_updates || update
+  fi
+
+  case "$1" in
+    start)
+      start $@
+      ;;
+    stop)
+      stop $@
+      ;;
+    restart)
+      restart $@
+      ;;
+    status)
+      status $@
+      ;;
+    install)
+      install $@
+      ;;
+    uninstall)
+      uninstall $@
+      ;;
+    update)
+      update $@
+      ;;
+    *)
+      usage $@
+      ;;
+  esac
 }
 
-main
+main $@; exit 0;
