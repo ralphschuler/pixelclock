5effe5a919c89d90f9b8d9e30aa23c52a595f1d1
Ralph Schuler <ralph.schuler@nyphon.de>
2023-01-24 22:23:24 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const matrix = new Matrix<WS281x<IWS281xOptions>, IWS281xOptions>(options);
 
 const colorAmount = 5
 const matricsEffect = new MatrixEffect({
-  colors: [...new Array(colorAmount)].map((index) =>
+  colors: [...new Array(colorAmount)].map((_, index) =>
     Color.fromHsl(Math.floor((index * (260 / colorAmount))), 100, 50)
   ),
   matrix: matrix,
